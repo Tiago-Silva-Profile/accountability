b1ee14b66507a424358db7b4e1f1ab75c938607a
Tiago.Antonio <tiago.antonio@mv.com.br>
2024-08-23 12:53:24 -0300

    [ACC-0002] Layout finalizado.

--- src/app/last-few-transactions/last-few-transactions.component.ts
+++ src/app/last-few-transactions/last-few-transactions.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-last-few-transactions',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './last-few-transactions.component.html',
   styleUrl: './last-few-transactions.component.css'
 })
